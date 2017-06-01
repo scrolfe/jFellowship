53ce2da6692e24eadae2c1dea672d0c822bc578c
Sam Rolfe <scrolfe@gmail.com>
2017-06-01 07:13:02 -0500

    to makeHobbits, stuck on adding list item content to <li> tags

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -17,37 +17,50 @@ var buddies = [
 ];
 
 var lands = ['The Shire', 'Rivendell', 'Mordor'];
-var body = document.body;
-var section = '<section></section>';
+// var body = document.body;
+// var section = '<section></section>';
 
 function makeMiddleEarth() {
-    // create a section tag with an id of `middle-earth`
-       // add each land as an `article` tag
-       // inside each `article` tag include an `h1` with the name of the land
-       // append `middle-earth` to your document `body`
+  // create a section tag with an id of `middle-earth`
+  $('body').append('<section></section>')
+  $('section').attr('id', 'middle-earth')
+  // add each land as an `article` tag
+  // inside each `article` tag include an `h1` with the name of the land
+  for (var i = 0; i < lands.length; i++) {
+    $('section').append('<article></article>')
+    $('article').eq(i).append('<h1></h1>')
+    $('h1').eq(i).text(lands[i])
+  }
+  // append `middle-earth` to your document `body`
+  $('body').append($('#middle-earth'))
 }
 makeMiddleEarth();
 
 function makeHobbits(){
   // display an `unordered list` of hobbits in the shire
-        // (which is the second article tag on the page)
-     // give each hobbit a class of `hobbit`
+  $('article').eq(0).append('<ul></ul>')
+  for (var i = 0; i < hobbits.length; i++)
+    $('ul').append('<li></li>')
+    $('li').eq(i).append(hobbits).eq(i)
+
+  // (which is the second article tag on the page)
+  // give each hobbit a class of `hobbit`
 }
 
 makeHobbits();
 
 function keepItSecretKeepItSafe(){
   // create a div with an id of `'the-ring'`
-     // give the div a class of `'magic-imbued-jewelry'`
-     // add the ring as a child of `Frodo`
+  // give the div a class of `'magic-imbued-jewelry'`
+  // add the ring as a child of `Frodo`
 }
 
 keepItSecretKeepItSafe();
 
 function makeBuddies(){
   // create an `aside` tag
-   // attach an `unordered list` of the `'buddies'` in the aside
-   // insert your aside as a child element of `rivendell`
+  // attach an `unordered list` of the `'buddies'` in the aside
+  // insert your aside as a child element of `rivendell`
 }
 
 makeBuddies();
@@ -66,15 +79,15 @@ leaveTheShire();
 
 function forgeTheFellowship() {
   // create a new div called `'the-fellowship'` within `rivendell`
-     // add each `hobbit` and `buddy` one at a time to `'the-fellowship'`
-     // after each character is added make an alert that they // have joined your party
+  // add each `hobbit` and `buddy` one at a time to `'the-fellowship'`
+  // after each character is added make an alert that they // have joined your party
 }
 
 forgeTheFellowship();
 
 function theBalrog(){
   // change the `'Gandalf'` text to `'Gandalf the White'`
-     // apply the following style to the element, make the // background 'white', add a grey border
+  // apply the following style to the element, make the // background 'white', add a grey border
 }
 
 theBalrog();
@@ -89,15 +102,15 @@ hornOfGondor();
 
 function itsDangerousToGoAlone() {
   // take `Frodo` and `Sam` out of the fellowship and move // them to `Mordor`
-    // add a div with an id of `'mount-doom'` to `Mordor`
+  // add a div with an id of `'mount-doom'` to `Mordor`
 }
 
 itsDangerousToGoAlone();
 
 function weWantsIt() {
   // Create a div with an id of `'gollum'` and add it to Mordor
- // Remove `the ring` from `Frodo` and give it to `Gollum`
- // Move Gollum into Mount Doom
+  // Remove `the ring` from `Frodo` and give it to `Gollum`
+  // Move Gollum into Mount Doom
 }
 
 weWantsIt();
